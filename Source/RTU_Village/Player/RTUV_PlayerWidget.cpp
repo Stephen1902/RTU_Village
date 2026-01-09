94409325c21438fbe28596ffeeee74da9b29f64a
Stephen Nichols <nichols0512@hotmail.com>
2026-01-09 16:58:22 +0000

    Added extra functionality to RTUV_PlayerWidget.h and RTUV_PlayerWidget.cpp

--- Source/RTU_Village/Player/RTUV_PlayerWidget.cpp
+++ Source/RTU_Village/Player/RTUV_PlayerWidget.cpp
@@ -1,4 +1,83 @@
 // Copyright 2026 DME Games
 
 #include "RTUV_PlayerWidget.h"
+#include "../Framework/RTUV_GameState.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
 
+void URTUV_PlayerWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	GameStateRef = Cast<ARTUV_GameState>(UGameplayStatics::GetGameState(GetWorld()));
+
+	BtnTreeReduce->OnClicked.AddDynamic(this, &URTUV_PlayerWidget::OnBtnTreeReduceClicked);
+	BtnTreeIncrease->OnClicked.AddDynamic(this, &URTUV_PlayerWidget::OnBtnTreeIncreaseClicked);
+
+	BtnDefenceReduce->OnClicked.AddDynamic(this, &URTUV_PlayerWidget::OnBtnDefenceReduceClicked);
+	BtnDefenceIncrease->OnClicked.AddDynamic(this, &URTUV_PlayerWidget::OnBtnDefenceIncreaseClicked);
+
+	BtnHuntReduce->OnClicked.AddDynamic(this, &URTUV_PlayerWidget::OnBtnHuntReduceClicked);
+	BtnHuntIncrease->OnClicked.AddDynamic(this, &URTUV_PlayerWidget::OnBtnHuntIncreaseClicked);
+
+	BtnFoodReduce->OnClicked.AddDynamic(this, &URTUV_PlayerWidget::OnBtnFoodReduceClicked);
+	BtnFoodIncrease->OnClicked.AddDynamic(this, &URTUV_PlayerWidget::OnBtnFoodIncreaseClicked);
+
+	BtnHousingReduce->OnClicked.AddDynamic(this, &URTUV_PlayerWidget::OnBtnHousingReduceClicked);
+	BtnHousingIncrease->OnClicked.AddDynamic(this, &URTUV_PlayerWidget::OnBtnHousingIncreaseClicked);
+	
+}
+
+void URTUV_PlayerWidget::NewDayStart(int32 CurrentDay, int32 TotalDays, int32 AvailablePeople, float DefenceComplete, int32 TreesStored, int32 RawFoodStored, int32 CookedFoodStored, int32 HousesBuilt)
+{
+	
+}
+
+void URTUV_PlayerWidget::NoAvailablePeople()
+{
+	BtnDefenceIncrease->SetIsEnabled(false);
+	BtnHuntIncrease->SetIsEnabled(false);
+	BtnFoodIncrease->SetIsEnabled(false);
+	BtnTreeIncrease->SetIsEnabled(false);
+	BtnHousingIncrease->SetIsEnabled(false);
+}
+
+void URTUV_PlayerWidget::OnBtnTreeReduceClicked()
+{
+}
+
+void URTUV_PlayerWidget::OnBtnTreeIncreaseClicked()
+{
+}
+
+void URTUV_PlayerWidget::OnBtnHuntReduceClicked()
+{
+}
+
+void URTUV_PlayerWidget::OnBtnHuntIncreaseClicked()
+{
+}
+
+void URTUV_PlayerWidget::OnBtnFoodReduceClicked()
+{
+}
+
+void URTUV_PlayerWidget::OnBtnFoodIncreaseClicked()
+{
+}
+
+void URTUV_PlayerWidget::OnBtnDefenceReduceClicked()
+{
+}
+
+void URTUV_PlayerWidget::OnBtnDefenceIncreaseClicked()
+{
+}
+
+void URTUV_PlayerWidget::OnBtnHousingReduceClicked()
+{
+}
+
+void URTUV_PlayerWidget::OnBtnHousingIncreaseClicked()
+{
+}
--- Source/RTU_Village/Player/RTUV_PlayerWidget.h
+++ Source/RTU_Village/Player/RTUV_PlayerWidget.h
@@ -6,6 +6,7 @@
 #include "Blueprint/UserWidget.h"
 #include "RTUV_PlayerWidget.generated.h"
 
+class UTextBlock;
 class UButton;
 
 /**
@@ -15,5 +16,108 @@ UCLASS()
 class RTU_VILLAGE_API URTUV_PlayerWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+	virtual void NativeConstruct() override;
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbDay;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbAvailablePeople;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbDefenceComplete;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbTreeStore;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbFoodStore;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbHousingAvailable;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnTreeReduce;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbTreeAssigned;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnTreeIncrease;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnHuntReduce;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbHuntAssigned;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnHuntIncrease;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnFoodReduce;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbFoodAssigned;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnFoodIncrease;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnDefenceReduce;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbDefenceAssigned;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnDefenceIncrease;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnHousingReduce;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UTextBlock* TbHousingAssigned;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnHousingIncrease;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Widget", meta=(BindWidget))
+	UButton* BtnBeginDay;
+
+public:
+	void NewDayStart(int32 CurrentDay, int32 TotalDays, int32 AvailablePeople, float DefenceComplete, int32 TreesStored, int32 RawFoodStored, int32 CookedFoodStored, int32 HousesBuilt);
+	
+private:
+	UPROPERTY()
+	class ARTUV_GameState* GameStateRef;
+
+	void NoAvailablePeople();
+
+	UFUNCTION()
+	void OnBtnTreeReduceClicked();
+	UFUNCTION()
+	void OnBtnTreeIncreaseClicked();
+
+	UFUNCTION()
+	void OnBtnHuntReduceClicked();
+	UFUNCTION()
+	void OnBtnHuntIncreaseClicked();
+
+	UFUNCTION()
+	void OnBtnFoodReduceClicked();
+	UFUNCTION()
+	void OnBtnFoodIncreaseClicked();
+
+	UFUNCTION()
+	void OnBtnDefenceReduceClicked();
+	UFUNCTION()
+	void OnBtnDefenceIncreaseClicked();
+
+	UFUNCTION()
+	void OnBtnHousingReduceClicked();
+	UFUNCTION()
+	void OnBtnHousingIncreaseClicked();
+
 	
 };
