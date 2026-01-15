9d3bad1acd6976e919e9a6ee6842aad706453ba7
Stephen Nichols <nichols0512@hotmail.com>
2026-01-15 13:04:10 +0000

    Added variables, and Enum and struct for tools to GameState.
    Created data table to be used for tools.
    
    Set up some default values.

--- Source/RTU_Village/Framework/RTUV_GameState.cpp
+++ Source/RTU_Village/Framework/RTUV_GameState.cpp
@@ -2,3 +2,34 @@
 
 #include "RTUV_GameState.h"
 
+ARTUV_GameState::ARTUV_GameState()
+{
+	// Game start items
+	CurrentDay = 1;
+	TreesStored = 0;
+	RawFoodStored = 5;
+	CookFoodStored = 5;
+	HousesBuilt = 2;
+
+	// Gameplay items
+	DefenceComplete = 0.f;
+	TreesPerPerson = 2;
+	TreesNeededForDefence = 0.5f;
+	DefencePerPerson = 2.0f;
+	RawFoodPerPerson = 5.0f;
+	RawFoodForCookedFood = 1.0f;
+	CookedFoodPerPerson = 4;
+	CookedFoodConsumed = 0.5f;
+	TreesNeededPerHouse = 8;
+	HouseBuiltPerPerson = 0.33f;
+
+	DaysBeforeUprising = 30;
+
+	Population = 4;
+
+	static ConstructorHelpers::FObjectFinder<UDataTable> DT(TEXT("/Game/Framework/DT_GameTools"));
+	if (DT.Succeeded())
+	{
+		ToolsDataTable = DT.Object;
+	}
+}
--- Source/RTU_Village/Framework/RTUV_GameState.h
+++ Source/RTU_Village/Framework/RTUV_GameState.h
@@ -3,15 +3,129 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "GameFramework/GameStateBase.h"
 #include "RTUV_GameState.generated.h"
 
 /**
  * 
  */
+
+UENUM() 
+enum class EToolEnum : uint8
+{
+	EAxe	UMETA(DisplayName="Axe"),
+	EShovel UMETA(DisplayName="Shovel"),
+	EHammer UMETA(DisplayName="Hammer"),
+	EBow	UMETA(DisplayName="Bow"),
+	EKnife	UMETA(DisplayName="Knife")
+};
+
+USTRUCT()
+struct FTools : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditDefaultsOnly, Category="Tools")
+	FText ToolName;
+
+	UPROPERTY(EditDefaultsOnly, Category="Tools")
+	FText ToolDescription;
+
+	UPROPERTY(EditDefaultsOnly, Category="Tools")
+	EToolEnum ToolType;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Tools")
+	float IncreaseMultiplier;
+
+	FTools()
+	{
+		ToolName = FText::FromString("Tool Name.");
+		ToolDescription = FText::FromString("Text of what this tool does goes here.");
+		ToolType = EToolEnum::EAxe;
+		IncreaseMultiplier = 0.5f;
+	}
+};
+
 UCLASS()
 class RTU_VILLAGE_API ARTUV_GameState : public AGameStateBase
 {
 	GENERATED_BODY()
+
+	ARTUV_GameState();
+protected:
+	// The number of days before end game happens
+	UPROPERTY(EditDefaultsOnly, Category = "Game Start")
+	int32 DaysBeforeUprising;
+
+	// How many trees are in storage at Begin Play
+	UPROPERTY(EditDefaultsOnly, Category = "Game Start")
+	int32 TreesStored;
+
+	// How much raw food is stored at Begin Play
+	UPROPERTY(EditDefaultsOnly, Category = "Game Start")
+	int32 RawFoodStored;
+
+	// How much cooked food is stored at Begin Play
+	UPROPERTY(EditDefaultsOnly, Category = "Game Start")
+	int32 CookFoodStored;
+
+	// How many houses have been built at Begin Play
+	UPROPERTY(EditDefaultsOnly, Category = "Game Start")
+	float HousesBuilt;
+
+	// Tools available at Begin Play
+	UPROPERTY(EditDefaultsOnly, Category = "Game Start")
+	TMap<EToolEnum, int32> AvailableTools;
+	
+	// The amount of defence completed
+	UPROPERTY(VisibleAnywhere, Category = "Gameplay")
+	float DefenceComplete;
+
+	// The number of people needed to collect one tree;
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	int32 TreesPerPerson;
+	
+	// The number of trees needed for 1% of defence
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	float TreesNeededForDefence;
+
+	// The number of people for 1% of defence
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	float DefencePerPerson;
+
+	// The number of raw food gathered by 1 person
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	int32 RawFoodPerPerson;
+	
+	// The number of raw food needed to make 1 cooked food
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	float RawFoodForCookedFood;
+
+	// The number of cooked food made by 1 person
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	int32 CookedFoodPerPerson;
+
+	// The amount of trees needed for 1 house
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	int32 TreesNeededPerHouse;
+	
+	// The amount of a house built by 1 person
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	float HouseBuiltPerPerson;
+	
+	// How much food each person eats
+	UPROPERTY(EditDefaultsOnly, Category = "Gameplay")
+	float CookedFoodConsumed;
+	
+private:
+	int32 CurrentDay;
+	int32 Population;
+
+	UPROPERTY()
+	UDataTable* ToolsDataTable;
+
+	
+	
 	
 };
